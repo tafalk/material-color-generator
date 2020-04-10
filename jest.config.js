7185b30f66614c20e9d7f6508baf2e7978b87876
100117 <vahdet.keskin@isbank.com.tr>
2020-04-10 15:41:55 +0300

    tett

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   transform: {
-    "^.+\\.(t|j)sx?$": "ts-jest",
+    "^.+\\.(t)sx?$": "ts-jest",
   },
   coverageDirectory: "./coverage/",
   collectCoverage: true,
   collectCoverageFrom: [
     "**/*.{js,jsx,ts,tsx}",
-    "!**/dist/**",
     "!**/node_modules/**",
     "!**/vendor/**",
   ],
